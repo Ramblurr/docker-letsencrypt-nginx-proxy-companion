6d603be65c0cfed6f3b6a1f1f53ee5429b9a722f
munsio <github@r2pi.net>
2016-08-25 23:05:48 +0200

    correcting wrong nginx container name

--- app/rancher.sh
+++ app/rancher.sh
@@ -2,11 +2,10 @@
 #load container name from rancher meta-data
 CONTAINER_NAME="r-$(curl -s rancher-metadata/latest/self/container/name)"
 NGINX_DOCKER_GEN_CONTAINER=${CONTAINER_NAME/letsencrypt/nginx-gen}
-NGINX_DOCKER_CONTAINER=${CONTAINER_NAME/letsencrypt/nginx}
+NGINX_DOCKER_CONTAINER=${CONTAINER_NAME/letsencrypt_/}
 
 export NGINX_DOCKER_GEN_CONTAINER
 export NGINX_DOCKER_CONTAINER
-
 
 #download docker-gen nginx-template
 curl https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl > /etc/nginx/docker-gen/nginx.tmpl
