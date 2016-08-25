03cb7e77d92e03f0a8732c0e808c53a312f6c2e3
munsio <github@r2pi.net>
2016-08-25 22:34:26 +0200

    add config for docker-gen and other stuff

--- app/rancher.sh
+++ app/rancher.sh
@@ -1,4 +1,16 @@
 
 #load container name from rancher meta-data
-CONTAINER_NAME=$(curl -s rancher-metadata/latest/self/container/name)
-export NGINX_DOCKER_GEN_CONTAINER=${CONTAINER_NAME/letsencrypt/nginx-gen}
+CONTAINER_NAME="r-$(curl -s rancher-metadata/latest/self/container/name)"
+NGINX_DOCKER_GEN_CONTAINER=${CONTAINER_NAME/letsencrypt/nginx-gen}
+NGINX_DOCKER_CONTAINER=${CONTAINER_NAME/letsencrypt/nginx}
+
+export NGINX_DOCKER_GEN_CONTAINER
+export NGINX_DOCKER_CONTAINER
+
+
+#download docker-gen nginx-template
+curl https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl > /etc/nginx/docker-gen/nginx.tmpl
+
+#create docker-gen config file
+echo "$NGINX_DOCKER_CONTAINER = 1" >> /app/docker-gen.cfg
+ln -s /app/docker-gen.cfg /etc/nginx/docker-gen/docker-gen.cfg
